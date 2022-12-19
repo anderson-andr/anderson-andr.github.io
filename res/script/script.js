049aacad2861a7c38db506726ac64a8c69fa5f2a
Anderson <anderson.souza@maven.com.br>
2022-12-19 15:56:18 -0300

    ajustes

--- res/script/script.js
+++ res/script/script.js
@@ -1,8 +1,21 @@
 
-$(document).ready(function(){
-    $('.your-class').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
-  });
+
+$(document).ready(
+    function() {
+            /*QUANDO O ICONE HAMBUQUE FOR CLICADO  VAI SER EXECUTADO OUTRA FUNÇÃO 
+            SELECIONA O ELEMENTO COM A CLASSE JS--NAV-ICON E VAI FICAR ESPERANDO ESSE ELEMENTO*/
+        $('.js--nav-icon').click(
+             function () {
+                var nav = $('.main-nav');
+
+                nav.slideToggle(200);
+
+             }
+
+        );
+
+    }
+
+    
+);
+
