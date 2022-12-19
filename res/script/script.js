0f750e03d02d8d711b13687ead70f9968b1c685a
Anderson <anderson.souza@maven.com.br>
2022-12-19 16:23:21 -0300

    ajustes

--- res/script/script.js
+++ res/script/script.js
@@ -19,3 +19,12 @@ $(document).ready(
     
 );
 
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    
+    });
+  });
